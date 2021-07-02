21bf21a8fd6a2429d6ab0fc97989de6aada54331
Mark Bridger <mark.bridger2@skybettingandgaming.com>
2021-07-02 17:03:44 +0100

    test bug branch

--- src/test/java/APItest.java
+++ src/test/java/APItest.java
@@ -32,6 +32,8 @@ public class APItest {
 
     private static Headers commonHeaders;
 
+    //this is bug branch
+
     public Headers getCommonHeaders() {
         Header header1 = new Header("authority", "automationintesting.online");
         Header header2 = new Header("accept", "application/json");
